dfbc4b85fd0fd086610609099d5a0abe67500227
FilmPissocom <filmpisso.lk.com@gmail.com>
2024-12-13 23:18:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "හෙලෝ !👋 {first}\n\n<b>Telegram හරහා 🎬 Movies සහ TV Shows 📺 Download කරගැනීමට පෙර අපගේ 📽 𝐅𝐈𝐋𝐌 𝐏𝐈𝐒𝐒𝐎ᶜᵒᵐ 📽🔥🇱🇰 Updates Channel එකට Join වෙලා Back 🔙 වෙලා ඇවිත් පහල Try Again 🔄 ක්ලික් කරන්න. 👇 \n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
