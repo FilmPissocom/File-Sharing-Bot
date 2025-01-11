efe0e776d97db11d7eec787c5f4896bfc6bc01f8
FilmPissocom <filmpisso.lk.com@gmail.com>
2025-01-11 14:54:24 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 My Name :</b> <a href='https://t.me/FileSharingXProBot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>Jishu Developer</a>",
+            text = f"<b>🌐 Website :</b> <a href='https://filmpisso.com'>𝐕𝐢𝐬𝐢𝐭 📽 𝐅𝐈𝐋𝐌 𝐏𝐈𝐒𝐒𝐎ᶜᵒᵐ 📽</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢Join Channel :</b> <a href='https://t.me/FilmPissocom'>📽 𝐅𝐈𝐋𝐌 𝐏𝐈𝐒𝐒𝐎ᶜᵒᵐ 📽🔥🇱🇰</a> \n<b>📢 Join Chat :</b> <a href='https://t.me/FilmPissoChat'>📽 𝐅𝐈𝐋𝐌 𝐏𝐈𝐒𝐒𝐎ᶜᵒᵐ 𝐂𝐡𝐚𝐭 📽🔥🇱🇰</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>Sadew Didulanga</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
